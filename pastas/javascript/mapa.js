5b20b1cf253d1532d66fe0e4a1c765fc4a83322f
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2023-05-01 20:05:07 -0300

    adicionando a função salvar

--- pastas/javascript/mapa.js
+++ pastas/javascript/mapa.js
@@ -41,3 +41,27 @@ function initMap() {
     listar(google.maps)
   }
 }
+
+function addMarker(evt){
+    marker.setPosition(evt.latLng);
+}
+
+async function salvar(){
+
+    const obj = {
+        nome: document.getElementById('nome').value,
+        local: document.getElementById('local').value,
+        lat: marker.getPosition().lat(),
+        lng: marker.getPosition().lng()
+    };
+    await fetch("http://localhost:8080/eventos",{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj)
+    }).then(response =>{alert('Salvo com sucesso')})
+    .catch(error => alert('Falha ao salvar!'));    
+
+}
